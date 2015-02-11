1be3b20a3ca67034688a52d8f4ba0bf9f32ebf2b
Jithin Jose <jithinjose1600@gmail.com>
2015-02-11 15:57:56 -0500

    Initial Commit

---
+++ src/java/servlet/ControllerServlet.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ *
+ * @author c0648991
+ */
+@WebServlet
+public class ControllerServlet extends HttpServlet{
+    
+    
+}
