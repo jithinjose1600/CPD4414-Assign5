59b11371b2ba754c372d252c3474aaa95e73ed41
Jithin Jose <jithinjose1600@gmail.com>
2015-02-23 21:42:20 -0500

    doPost

--- src/java/servlet/ControllerServlet.java
+++ src/java/servlet/ControllerServlet.java
@@ -13,6 +13,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.annotation.WebServlet;
@@ -34,9 +35,9 @@ public class ControllerServlet extends HttpServlet{
         
        StringBuilder sb = new StringBuilder();
        String query=null;
-        
+        PrintWriter out = res.getWriter();
         try (Connection conn = DBClass.getConnection()) {
-            PrintWriter out = res.getWriter();
+            
             PreparedStatement pstmt=null;
             if (!req.getParameterNames().hasMoreElements()) {
                 query="SELECT * FROM PRODUCTS";
@@ -64,5 +65,43 @@ public class ControllerServlet extends HttpServlet{
         }  
     }
     
-  
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Set<String> keySet = request.getParameterMap().keySet(); 
+        PrintWriter out = response.getWriter();
+        String query=null;
+        int res=0;
+        out.println("before if");
+        if (keySet.contains("name") && keySet.contains("description") && keySet.contains("quantity")) {               
+                String name = request.getParameter("name");
+                out.println(name);
+                String description= request.getParameter("description");
+                out.println(description);
+                String quantity=request.getParameter("quantity");
+                out.println(quantity);
+                try (Connection conn = DBClass.getConnection()) {
+                query="INSERT INTO PRODUCTS (Name, Description, Quantity) VALUES (?, ?, ?)";
+                PreparedStatement pstmt = conn.prepareStatement(query);
+                pstmt.setString(1, name);
+                pstmt.setString(2, description);
+                pstmt.setString(3, quantity);
+                out.println("before update");
+                res=pstmt.executeUpdate();
+                out.println("after update");
+                if(res>0)
+                {
+                   out.println("Successfully inserted!!");
+                }
+                else
+                {
+                    response.setStatus(500); 
+                }
+                 } catch (SQLException ex) {
+            Logger.getLogger(ControllerServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            } else {
+               out.println("Error: Not enough data to input. Please use a URL of the form /servlet?name=XXX&description=XXX&quantity=XX");
+                   }
+            
+    }
 }
